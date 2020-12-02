e09bc07b2425e59370b08b1c3dd8b4a02d0fff0b
Chris Hoban <christopher.hoban@nttdata.com>
2020-12-02 12:38:52 -0800

    First pass at shipping in exported objects
    30% success rate

--- openshift/migration/oc3-oc4-migrator.bat
+++ openshift/migration/oc3-oc4-migrator.bat
@@ -1,15 +1,17 @@
 @echo on
-
-REM collect the params
+setlocal EnableDelayedExpansion
+REM #### collect the params ####
 
 set /p oc3_token=Openshift 3 Login Token:
 set /p oc3_ns=Openshift 3 License Plate:
 set /p oc3_projectname=Project Name:
 
+
+REM ###### LOGIN TO OC3 ########
 oc3 login https://console.pathfinder.gov.bc.ca:8443 --token=%oc3_token%
 
-REM create all the dirs
-cd ../migration-scripts
+REM #### create all the dirs branching off from where you placed/ran this script #####
+
 mkdir %oc3_projectname%-export
 cd %oc3_projectname%-export
 mkdir %oc3_projectname%-tools
@@ -21,12 +23,12 @@ REM ##### Openshift 3 Tools ######
 cd %oc3_projectname%-tools
 oc3 project %oc3_ns%-tools
 
-REM Get the legacy main object backup (RAW)
+REM #### Get the legacy main object backup (RAW) ####
 oc3 get -o yaml --export all > %oc3_projectname%-rawbackup.yaml
 
 oc3 get services --export > services
 
-REM Backup discrete kubernetes templates by object type
+REM #### Backup discrete kubernetes templates by object type ####
 clear pods --as-template=%oc3_projectname%-pods > %oc3_projectname%-pods.yaml
 oc3 export replicationcontrollers --as-template=%oc3_projectname%-replicationcontrollers > %oc3_projectname%-replicationcontrollers.yaml
 oc3 export services --as-template=%oc3_projectname%-services > %oc3_projectname%-services.yaml
@@ -54,14 +56,14 @@ oc3 export replicasets --as-template=%oc3_projectname%-replicasets > %oc3_projec
 oc3 export poddisruptionbudget --as-template=%oc3_projectname%-poddisruptionbudget > %oc3_projectname%-poddisruptionbudget.yaml
 oc3 export endpoints --as-template=%oc3_projectname%-endpoints > %oc3_projectname%-endpoints.yaml
 
-REM Get back to parent directory for next stage
+REM##  Get back to parent directory for next stage
 cd ..
 
 REM ##### Openshift 3 Dev ######
 cd %oc3_projectname%-dev
 oc3 project %oc3_ns%-dev
 
-REM Get the legacy main object backup (RAW)
+REM #### Get the legacy main object backup (RAW) ####
 oc3 get -o yaml --export all > %oc3_projectname%-rawbackup.yaml
 
 REM Backup discrete kubernetes templates by object type
@@ -92,14 +94,14 @@ oc3 export replicasets --as-template=%oc3_projectname%-replicasets > %oc3_projec
 oc3 export poddisruptionbudget --as-template=%oc3_projectname%-poddisruptionbudget > %oc3_projectname%-poddisruptionbudget.yaml
 oc3 export endpoints --as-template=%oc3_projectname%-endpoints > %oc3_projectname%-endpoints.yaml
 
-REM Get back to parent directory for next stage
+REM ### Get back to parent directory for next stage ###
 cd ..
 
 REM ##### Openshift 3 Test ######
 cd %oc3_projectname%-test
 oc3 project %oc3_ns%-test
 
-REM Get the legacy main object backup (RAW)
+REM ### Get the legacy main object backup (RAW) ###
 oc3 get -o yaml --export all > %oc3_projectname%-rawbackup.yaml
 
 REM Backup discrete kubernetes templates by object type
@@ -130,17 +132,17 @@ oc3 export replicasets --as-template=%oc3_projectname%-replicasets > %oc3_projec
 oc3 export poddisruptionbudget --as-template=%oc3_projectname%-poddisruptionbudget > %oc3_projectname%-poddisruptionbudget.yaml
 oc3 export endpoints --as-template=%oc3_projectname%-endpoints > %oc3_projectname%-endpoints.yaml
 
-REM Get back to parent directory for next stage
+REM ### Get back to parent directory for next stage ###
 cd ..
 
 REM ##### Openshift 3 Prod ######
 cd %oc3_projectname%-prod
 oc3 project %oc3_ns%-prod
 
-REM Get the legacy main object backup (RAW)
+REM ### Get the legacy main object backup (RAW) ###
 oc3 get -o yaml --export all > %oc3_projectname%-rawbackup.yaml
 
-REM Backup discrete kubernetes templates by object type
+REM ### Backup discrete kubernetes templates by object type ###
 oc3 export pods --as-template=%oc3_projectname%-pods > %oc3_projectname%-pods.yaml
 oc3 export replicationcontrollers --as-template=%oc3_projectname%-replicationcontrollers > %oc3_projectname%-replicationcontrollers.yaml
 oc3 export services --as-template=%oc3_projectname%-services > %oc3_projectname%-services.yaml
@@ -167,44 +169,79 @@ oc3 export deployments --as-template=%oc3_projectname%-deployments > %oc3_projec
 oc3 export replicasets --as-template=%oc3_projectname%-replicasets > %oc3_projectname%-replicasets.yaml
 oc3 export poddisruptionbudget --as-template=%oc3_projectname%-poddisruptionbudget > %oc3_projectname%-poddisruptionbudget.yaml
 oc3 export endpoints --as-template=%oc3_projectname%-endpoints > %oc3_projectname%-endpoints.yaml
-
-cd ..
 cd ..
 
 REM #### Logging out of Openshift 3 ####
-
 oc3 logout
 
 REM #### Collect Openshift 4 Parameters ####
-
 set /p oc4_token=Openshift 4 Login Token:
 set /p oc4_ns=Openshift License Plate:
 
 oc4 login --token=%oc4_token% --server=https://api.silver.devops.gov.bc.ca:6443
 
-REM #### List Location ####
-
-dir
-
-REM #### Move to Export Folder ####
-
-cd %oc3_projectname%-export
-
-REM #### List Location ####
-
-dir
 
 REM ##### Move to Openshift 3 Tools Export Directory #####
 
 oc4 project %oc4_ns%-tools
 
-REM ##### Process and Create Build Templates #####
 
+REM ### Import Tools First ###
 cd %oc3_projectname%-tools
+REM ##### Process and Create Build Configs #####
 oc4 process -f %oc3_projectname%-buildconfigs.yaml -l migration=%oc3_projectname%-migration-template | oc4 create -f -
-oc4 get buildconfig
+REM ##### Process and Create ImageStreams #####
+oc4 process -f %oc3_projectname%-imagestreams.yaml -l migration=%oc3_projectname%-migration-template | oc4 create -f -
+REM ##### Process and Create services #####
+oc4 process -f %oc3_projectname%-services.yaml -l migration=%oc3_projectname%-migration-template | oc4 create -f -
+REM ##### Process and Create ConfigMaps #####
+oc4 process -f %oc3_projectname%-cm.yaml -l migration=%oc3_projectname%-migration-template | oc4 create -f -
+REM ##### Process and Create Secrets #####
+oc4 process -f %oc3_projectname%-secrets.yaml -l migration=%oc3_projectname%-migration-template | oc4 create -f -
+REM ##### Process and Create egressnetworkpolicies #####
+oc4 process -f %oc3_projectname%-egressnetworkpolicies.yaml -l migration=%oc3_projectname%-migration-template | oc4 create -f -
+REM ##### Process and Create statefulsets #####
+oc4 process -f %oc3_projectname%-statefulsets.yaml -l migration=%oc3_projectname%-migration-template | oc4 create -f -
+REM ##### Process and Create cronjobs #####
+oc4 process -f %oc3_projectname%-cronjobs.yaml -l migration=%oc3_projectname%-migration-template | oc4 create -f -
+REM ##### Process and Create serviceaccounts #####
+oc4 process -f %oc3_projectname%-serviceaccounts.yaml -l migration=%oc3_projectname%-migration-template | oc4 create -f -
+REM ##### Process and Create rolebindings #####
+oc4 process -f %oc3_projectname%-rolebindings.yaml -l migration=%oc3_projectname%-migration-template | oc4 create -f -
+######REM ##### Process and Create endpoints #####
+######oc4 process -f %oc3_projectname%-endpoints.yaml -l migration=%oc3_projectname%-migration-template | oc4 create -f -
+REM ##### Process and Create deploymentconfigs #####
+oc4 process -f %oc3_projectname%-deploymentconfigs.yaml -l migration=%oc3_projectname%-migration-template | oc4 create -f -
+###REM ##### Process and Create replicationcontrollers #####
+###oc4 process -f %oc3_projectname%-replicationcontrollers.yaml -l migration=%oc3_projectname%-migration-template | oc4 create -f -
+REM ##### Process and Create builds #####
+oc4 process -f %oc3_projectname%-builds.yaml -l migration=%oc3_projectname%-migration-template | oc4 create -f -
+######REM ##### Process and Createimagestreamtags #####
+######oc4 process -f %oc3_projectname%-imagestreamtags.yaml -l migration=%oc3_projectname%-migration-template | oc4 create -f -
+REM ##### Process and Create rolebindingrestrictions #####
+oc4 process -f %oc3_projectname%-rolebindingrestrictions.yaml -l migration=%oc3_projectname%-migration-template | oc4 create -f -
+###REM ##### Process and Create limitranges #####
+###oc4 process -f %oc3_projectname%-limitranges.yaml -l migration=%oc3_projectname%-migration-template | oc4 create -f -
+###REM ##### Process and Create resourcequotas #####
+###oc4 process -f %oc3_projectname%-resourcequotas.yaml -l migration=%oc3_projectname%-migration-template | oc4 create -f -
+REM ##### Process and Create pvc #####
+oc4 process -f %oc3_projectname%-pvc.yaml -l migration=%oc3_projectname%-migration-template | oc4 create -f -
+REM ##### Process and Create hpa #####
+oc4 process -f %oc3_projectname%-hpa.yaml -l migration=%oc3_projectname%-migration-template | oc4 create -f -
+REM ##### Process and Create templates #####
+oc4 process -f %oc3_projectname%-templates.yaml -l migration=%oc3_projectname%-migration-template | oc4 create -f -
+REM ##### Process and Create deployments #####
+oc4 process -f %oc3_projectname%-deployments.yaml -l migration=%oc3_projectname%-migration-template | oc4 create -f -
+REM ##### Process and Create replicasets #####
+oc4 process -f %oc3_projectname%-replicasets.yaml -l migration=%oc3_projectname%-migration-template | oc4 create -f -
+REM ##### Process and Create poddisruptionbudget #####
+oc4 process -f %oc3_projectname%-poddisruptionbudget.yaml -l migration=%oc3_projectname%-migration-template | oc4 create -f -
+REM ##### Process and Create pods #####
+oc4 process -f %oc3_projectname%-pods.yaml -l migration=%oc3_projectname%-migration-template | oc4 create -f -
 
-pause 
+### REM Press Enter to Rollback, or CTRL-C and Y to quit, (Rollback is not for all objects yet)
+pause
+
 oc4 delete all --selector migration=%oc3_projectname%-migration-template
 
 REM #### Logout of Openshift 4 ####
---
+++ openshift/migration/readme.md
@@ -0,0 +1,17 @@
+# OpenShift Meta Template
+
+OpenShift 3 to OpenShift 4 migration tool is a simple approach to use the OC3 ClI and OC4 CLI to export templated kubernetes objects from OC3 Pathfinder project namespaces (tools, dev, test, prod) for one project, and then Import Kubernetes objects into OC4. It is in its early stages and will likely not cover an entire project migration. What its goal is, is to automate the tedium and start your OC4 namespace with specific builds, buildconfigs, deploymentconfigs, configmaps, secrets all populated. The idea is that the effort becomes restoring build pipelines in Tools with what is moved accross, such that real builds and deployments can be done to roll everything to a working state straight to Prod.
+
+
+## Getting Started/Pre requisites
+
+- Requires both OpenShift 3 CLI and the OpenShift 4 Cli
+  - find oc3 cli (use your path environment variable if you forgot)
+  - Rename to oc3.
+  - Install OC4 CLI, in a new location.
+  - Rename the binary to oc4, make sure its also in your path environment variable
+
+
+## Notes
+
+- Work in progress, may already provide value, but several object translation and a cleaner generation of services like Jenkins is in progress. This all may soon move to Ansible as well, though for now the .bat file approach is generally the lowest barrier to entry for now.
