692a8f1db6a0004ef94066bdd03faaac65369a76
Chris Hoban <christopher.hoban@nttdata.com>
2021-02-09 13:41:02 -0800

    Addition of role bindings template

--- openshift/migration/oc3-oc4-migrator.sh
+++ openshift/migration/oc3-oc4-migrator.sh
@@ -18,26 +18,32 @@ kubernetes_obj+=('imagestreams')
 kubernetes_obj+=('services')
 kubernetes_obj+=('cm')
 kubernetes_obj+=('secrets')
-kubernetes_obj+=('egressnetworkpolicies')
-kubernetes_obj+=('statefulsets')
-kubernetes_obj+=('cronjobs')
-kubernetes_obj+=('serviceaccounts')
-kubernetes_obj+=('rolebindings')
-kubernetes_obj+=('endpoints')
 kubernetes_obj+=('deploymentconfigs')
-kubernetes_obj+=('replicationcontrollers')
-kubernetes_obj+=('builds')
-kubernetes_obj+=('imagestreamtags')
-kubernetes_obj+=('rolebindingrestrictions')
-kubernetes_obj+=('limitranges')
-kubernetes_obj+=('resourcequotas')
-kubernetes_obj+=('pvc')
-kubernetes_obj+=('templates')
-kubernetes_obj+=('hpa')
-kubernetes_obj+=('deployments')
-kubernetes_obj+=('replicasets')
-kubernetes_obj+=('poddisruptionbudget')
-kubernetes_obj+=('pods')
+#kubernetes_obj+=('egressnetworkpolicies')
+#kubernetes_obj+=('statefulsets')
+#kubernetes_obj+=('cronjobs')
+#kubernetes_obj+=('serviceaccounts')
+#kubernetes_obj+=('rolebindings')
+#kubernetes_obj+=('endpoints')
+#kubernetes_obj+=('replicationcontrollers')
+#kubernetes_obj+=('builds')
+#kubernetes_obj+=('imagestreamtags')
+#kubernetes_obj+=('rolebindingrestrictions')
+#kubernetes_obj+=('limitranges')
+#kubernetes_obj+=('resourcequotas')
+#kubernetes_obj+=('pvc')
+#kubernetes_obj+=('templates')
+#kubernetes_obj+=('hpa')
+#kubernetes_obj+=('deployments')
+#kubernetes_obj+=('replicasets')
+#kubernetes_obj+=('poddisruptionbudget')
+#kubernetes_obj+=('pods')
+
+
+environments=()
+environments+=('dev')
+environments+=('test')
+environments+=('prod')
 
 
 echo #### Just in case you have an oc4 session ####
@@ -49,67 +55,26 @@ echo #### create all the dirs branching off from where you placed/ran this scrip
 
 mkdir ${oc3_projectname}-export
 cd ${oc3_projectname}-export
-mkdir ${oc3_projectname}-tools
-mkdir ${oc3_projectname}-dev
-mkdir ${oc3_projectname}-test
-mkdir ${oc3_projectname}-prod
-
-echo ##### Openshift 3 Tools ######
-cd ${oc3_projectname}-tools
-oc3 project ${oc3_ns}-tools
-
-echo #### Get the legacy main object backup (RAW) ####
-oc3 get -o yaml --export all > ${oc3_projectname}-rawbackup.yaml
-
-echo #### Backup discrete kubernetes templates by object type ####
-for t in ${kubernetes_obj[@]}; do
-  oc3 export ${t} --as-template=${oc3_projectname}-${t} > ${oc3_projectname}-${t}.yaml
+for d in ${environments[@]}; do
+  mkdir ${oc3_projectname}-${d}
 done
 
-echo##  Get back to parent directory for next stage
-cd ..
+for d in ${environments[@]}; do
+  echo ##### Openshift 3 ${d} ######
+  cd ${oc3_projectname}-${d}
+  oc3 project ${oc3_ns}-${d}
 
-echo ##### Openshift 3 Dev ######
-cd ${oc3_projectname}-dev
-oc3 project ${oc3_ns}-dev
+  echo #### Get the legacy main object backup (RAW) ####
+  oc3 get -o yaml --export all > ${oc3_projectname}-rawbackup.yaml
 
-echo #### Get the legacy main object backup (RAW) ####
-oc3 get -o yaml --export all > ${oc3_projectname}-rawbackup.yaml
+  echo #### Backup discrete kubernetes templates by object type ####
+  for t in ${kubernetes_obj[@]}; do
+    oc3 export ${t} --as-template=${oc3_projectname}-${t} > ${oc3_projectname}-${t}.yaml
+  done
 
-echo #### Backup discrete kubernetes templates by object type ####
-for t in ${kubernetes_obj[@]}; do
-  oc3 export ${t} --as-template=${oc3_projectname}-${t} > ${oc3_projectname}-${t}.yaml
+  echo##  Get back to parent directory for next stage
+  cd ..
 done
-echo ### Get back to parent directory for next stage ###
-cd ..
-
-echo ##### Openshift 3 Test ######
-cd ${oc3_projectname}-test
-oc3 project ${oc3_ns}-test
-
-echo ### Get the legacy main object backup (RAW) ###
-oc3 get -o yaml --export all > ${oc3_projectname}-rawbackup.yaml
-
-echo #### Backup discrete kubernetes templates by object type ####
-for t in ${kubernetes_obj[@]}; do
-  oc3 export ${t} --as-template=${oc3_projectname}-${t} > ${oc3_projectname}-${t}.yaml
-done
-
-echo ### Get back to parent directory for next stage ###
-cd ..
-
-echo ##### Openshift 3 Prod ######
-cd ${oc3_projectname}-prod
-oc3 project ${oc3_ns}-prod
-
-echo ### Get the legacy main object backup (RAW) ###
-oc3 get -o yaml --export all > ${oc3_projectname}-rawbackup.yaml
-
-echo #### Backup discrete kubernetes templates by object type ####
-for t in ${kubernetes_obj[@]}; do
-  oc3 export ${t} --as-template=${oc3_projectname}-${t} > ${oc3_projectname}-${t}.yaml
-done
-cd ..
 
 echo #### Logging out of Openshift 3 ####
 oc3 logout
@@ -120,24 +85,27 @@ read -p 'Openshift 4 License Plate: ' oc4_ns
 
 oc4 login --token=${oc4_token} --server=https://api.silver.devops.gov.bc.ca:6443
 
+for d in ${environments[@]}; do
+  echo ##### Move to Openshift 3 ${d} Export Directory #####
 
-echo ##### Move to Openshift 3 Tools Export Directory #####
-
-oc4 project ${oc4_ns}-tools
+  oc4 project ${oc4_ns}-${d}
 
 
-echo ### Import Tools First ###
-cd ${oc3_projectname}-tools
+  echo ### Import Tools First ###
+  cd ${oc3_projectname}-${d}
 
-echo #### Backup discrete kubernetes templates by object type ####
-for t in ${kubernetes_obj[@]}; do
-  echo ##### Process and Create ${t} #####
-  oc4 process -f ${oc3_projectname}-${t}.yaml -l migration=${oc3_projectname}-migration-template | oc4 create -f -
+  echo #### Backup discrete kubernetes templates by object type ####
+  for t in ${kubernetes_obj[@]}; do
+    echo ##### Process and Create ${t} #####
+    oc4 process -f ${oc3_projectname}-${t}.yaml -l migration=${oc3_projectname}-migration-template | oc4 create -f -
+  done
+#  pause ' Press Enter to Rollback, or CTRL-C and Y to quit, (Rollback is not for all objects yet)'
+  cd ..
 done
 
-pause ' Press Enter to Rollback, or CTRL-C and Y to quit, (Rollback is not for all objects yet)'
 
-oc4 delete all --selector migration=${oc3_projectname}-migration-template
+
+#oc4 delete all --selector migration=${oc3_projectname}-migration-template
 
 echo #### Logout of Openshift 4 ####
 oc4 logout
