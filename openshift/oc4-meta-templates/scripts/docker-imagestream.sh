ae4bd2958f63e8ec4db99d8aeea8705b49e6657b
Chris Hoban <christopher.hoban@nttdata.com>
2021-08-30 09:49:11 -0700

    Formal completion of locak docker imagestream script

--- openshift/oc4-meta-templates/scripts/docker-imagestream.sh
+++ openshift/oc4-meta-templates/scripts/docker-imagestream.sh
@@ -7,22 +7,22 @@ read -p 'Imagestream Name (ie rabbitmq): ' imagestreamName
 read -p 'Docker User: ' dockerUser
 read -sp 'Docker Credential: ' dockerCred
 echo
-dockerPullSecret = ${imagestreamName}-docker-creds
+dockerPullSecret="${imagestreamName}-docker-creds"
 
-oc4 login --token=${oc4_token} --server=https://api.silver.devops.gov.bc.ca:6443
-oc4 project ${oc4_ns}
+oc login --token=${oc4_token} --server=https://api.silver.devops.gov.bc.ca:6443
+oc project ${oc4_ns}
 
-oc4 create secret docker-registry ${dockerPullSecret} \
+oc create secret docker-registry ${dockerPullSecret} \
     --docker-server=docker.io \
     --docker-username=${dockerUser} \
     --docker-password=${dockerCred} \
     --docker-email=unused
 
 echo 'Secret link to Service Accounts'
-oc4 secrets link default ${dockerPullSecret} --for=pull
-oc4 secrets link builder ${dockerPullSecret}
+oc secrets link default ${dockerPullSecret} --for=pull
+oc secrets link builder ${dockerPullSecret}
 
 echo 'Secret Image Template Process'
-oc4 process -f docker-image.yaml -p namespace=${oc4_ns} -p dockerImage=${dockerImage} -p imagestreamName=${imagestreamName} -p dockerPullSecret=${dockerPullSecret}| oc4 create -f -
+oc process -f docker-image.yaml -p namespace=${oc4_ns} -p dockerImage=${dockerImage} -p imagestreamName=${imagestreamName} -p dockerPullSecret=${dockerPullSecret}| oc4 create -f -
 
-oc4 logout
+oc logout
